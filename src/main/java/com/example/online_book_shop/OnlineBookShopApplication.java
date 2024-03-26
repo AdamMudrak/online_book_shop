35f619d344d32fe486b8e3c47449759517afb951
AdamMudrak <mudrets2000@gmail.com>
2024-03-26 21:38:09 +0200

    Initial commit

---
+++ src/main/java/com/example/online_book_shop/OnlineBookShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_book_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookShopApplication.class, args);
+	}
+
+}
