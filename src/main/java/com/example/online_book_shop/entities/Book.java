3645ab750605f2228f41352f397cf2e13f734597
AdamMudrak <mudrets2000@gmail.com>
2024-03-28 14:56:00 +0200

    Add model

---
+++ src/main/java/com/example/online_book_shop/entities/Book.java
@@ -0,0 +1,96 @@
+package com.example.online_book_shop.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+public class Book {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @NotNull(message = "Title may not be null")
+  private String title;
+  @NotNull(message = "Author may not be null")
+  private String author;
+  @NotNull(message = "isbn may not be null")
+  @Column(unique = true)
+  private String isbn;
+  @NotNull(message = "Price may not be null")
+  private BigDecimal price;
+  private String description;
+  private String coverImage;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getCoverImage() {
+    return coverImage;
+  }
+
+  public void setCoverImage(String coverImage) {
+    this.coverImage = coverImage;
+  }
+
+  @Override
+  public String toString() {
+    return "Book{"
+            + "id=" + id
+            + ", title='" + title + '\''
+            + ", author='" + author + '\''
+            + ", isbn='" + isbn + '\''
+            + ", price=" + price
+            + ", description='" + description + '\''
+            + ", coverImage='" + coverImage + '\''
+            + '}';
+  }
+}
