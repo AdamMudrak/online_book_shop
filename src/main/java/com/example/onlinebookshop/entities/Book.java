c2024632c2a1702ee1e8666b11b764f94bcec78d
AdamMudrak <mudrets2000@gmail.com>
2024-04-09 23:34:31 +0300

    Exclude boilerplate code from the model

--- src/main/java/com/example/onlinebookshop/entities/Book.java
+++ src/main/java/com/example/onlinebookshop/entities/Book.java
@@ -6,7 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
+import lombok.Data;
 
+@Data
 @Entity
 public class Book {
     @Id
@@ -23,73 +25,4 @@ public class Book {
     @Column(length = 3000)
     private String description;
     private String coverImage;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCoverImage() {
-        return coverImage;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" + System.lineSeparator()
-                + "id=" + id + ';' + System.lineSeparator()
-                + "title='" + title + "';" + System.lineSeparator()
-                + "author='" + author + "';" + System.lineSeparator()
-                + "isbn='" + isbn + "';" + System.lineSeparator()
-                + "price=" + price + ';' + System.lineSeparator()
-                + "description='" + description + "';" + System.lineSeparator()
-                + "coverImage='" + coverImage + "';" + System.lineSeparator()
-                + '}';
-    }
 }
