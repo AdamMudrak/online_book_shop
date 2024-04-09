a52071636ddbcc1edd97c4e99abf324d595abb39
AdamMudrak <mudrets2000@gmail.com>
2024-04-09 23:43:29 +0300

    Add MapperConfig and mapstruct dependency

---
+++ src/main/java/com/example/onlinebookshop/config/MapperConfig.java
@@ -0,0 +1,13 @@
+package com.example.onlinebookshop.config;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.NullValueCheckStrategy;
+
+@org.mapstruct.MapperConfig(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        implementationName = "<PACKAGE_NAME>.impl"
+)
+public class MapperConfig {
+}
