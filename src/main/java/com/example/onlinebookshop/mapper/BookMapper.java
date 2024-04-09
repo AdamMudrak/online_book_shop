b68db60353a9a536b53d7be13141a787461a40d0
AdamMudrak <mudrets2000@gmail.com>
2024-04-10 02:51:18 +0300

    Fix a naming mistake

--- src/main/java/com/example/onlinebookshop/mapper/BookMapper.java
+++ src/main/java/com/example/onlinebookshop/mapper/BookMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
-    BookDto toDto(Book employee);
+    BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
 }
