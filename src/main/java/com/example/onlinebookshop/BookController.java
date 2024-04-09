feda9ed31f873b125aad0bb1cbbf0125e5e0132a
AdamMudrak <mudrets2000@gmail.com>
2024-04-10 00:53:29 +0300

    Add multiple imports instead of single

--- src/main/java/com/example/onlinebookshop/BookController.java
+++ src/main/java/com/example/onlinebookshop/BookController.java
@@ -5,7 +5,11 @@ import com.example.onlinebookshop.dto.CreateBookRequestDto;
 import com.example.onlinebookshop.services.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
