513a10a7abd31233ad1853e23ac6d42862328688
AdamMudrak <mudrets2000@gmail.com>
2024-04-10 12:23:56 +0300

    Return test class - somewhy app refused to start with it, there were some strange checkstyle mistakes which weren't really there...

---
+++ src/test/java/com/example/onlinebookshop/OnlineBookShopApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.onlinebookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookShopApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
