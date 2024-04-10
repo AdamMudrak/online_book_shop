639f058a7a3ea85dae1d37693ec56f0dacfb1370
AdamMudrak <mudrets2000@gmail.com>
2024-04-10 12:12:08 +0300

    Add missing annotation

--- src/main/java/com/example/onlinebookshop/controller/BookController.java
+++ src/main/java/com/example/onlinebookshop/controller/BookController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 }
