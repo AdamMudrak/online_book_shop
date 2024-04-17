424a15f312ca0593c00d73024fe216ace98406e9
AdamMudrak <mudrets2000@gmail.com>
2024-04-18 00:17:28 +0300

    Add @Component, move to a better named package

--- src/main/java/com/example/onlinebookshop/repositories/book/bookspecs/bookfieldsspecs/AuthorSpecificationProvider.java
+++ src/main/java/com/example/onlinebookshop/repositories/book/bookspecs/bookfieldsspecs/AuthorSpecificationProvider.java
@@ -1,11 +1,12 @@
-package com.example.onlinebookshop.repositories.book.spec;
+package com.example.onlinebookshop.repositories.book.bookspecs.bookfieldsspecs;
 
 import com.example.onlinebookshop.entities.Book;
-import com.example.onlinebookshop.repositories.SpecificationProvider;
-
+import com.example.onlinebookshop.repositories.specifications.SpecificationProvider;
 import java.util.Arrays;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthorSpecificationProvider implements SpecificationProvider<Book> {
     @Override
     public String getKey() {
--- src/main/java/com/example/onlinebookshop/repositories/book/bookspecs/bookfieldsspecs/ToPriceSpecificationProvider.java
+++ src/main/java/com/example/onlinebookshop/repositories/book/bookspecs/bookfieldsspecs/ToPriceSpecificationProvider.java
@@ -1,10 +1,12 @@
-package com.example.onlinebookshop.repositories.book.spec;
+package com.example.onlinebookshop.repositories.book.bookspecs.bookfieldsspecs;
 
 import com.example.onlinebookshop.entities.Book;
-import com.example.onlinebookshop.repositories.SpecificationProvider;
+import com.example.onlinebookshop.repositories.specifications.SpecificationProvider;
 import java.math.BigDecimal;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ToPriceSpecificationProvider implements SpecificationProvider<Book> {
     @Override
     public String getKey() {
