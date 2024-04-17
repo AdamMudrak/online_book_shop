58b958a1f3304ed9631fe1a88e277df21ef59874
AdamMudrak <mudrets2000@gmail.com>
2024-04-17 21:02:21 +0300

    Modify BookRepo

---
+++ src/main/java/com/example/onlinebookshop/repositories/book/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.onlinebookshop.repositories.book;
+
+import com.example.onlinebookshop.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+
+}
