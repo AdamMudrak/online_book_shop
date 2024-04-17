f4f152934e54073f48db09aca6a0db89f0d7d665
AdamMudrak <mudrets2000@gmail.com>
2024-04-17 17:54:36 +0300

    Add needed endpoints

--- src/main/java/com/example/onlinebookshop/controller/BookController.java
+++ src/main/java/com/example/onlinebookshop/controller/BookController.java
@@ -5,11 +5,15 @@ import com.example.onlinebookshop.dto.CreateBookRequestDto;
 import com.example.onlinebookshop.services.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -29,7 +33,20 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
-        return bookService.save(bookDto);
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDto createBook(@RequestBody CreateBookRequestDto bookRequestDto) {
+        return bookService.save(bookRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@RequestBody CreateBookRequestDto bookRequestDto,
+                              @PathVariable Long id) {
+        return bookService.update(bookRequestDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        bookService.delete(id);
     }
 }
