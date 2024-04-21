f552a31c71171a71bfb8f014e41c598bf38c4dc8
AdamMudrak <mudrets2000@gmail.com>
2024-04-21 23:59:07 +0300

    Add constraints for client

--- src/main/java/com/example/onlinebookshop/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/onlinebookshop/dto/CreateBookRequestDto.java
@@ -1,16 +1,30 @@
 package com.example.onlinebookshop.dto;
 
+import com.example.onlinebookshop.validation.PathToFile;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @NotBlank
+    @Size(min = 10, max = 13)
     private String isbn;
+    @NotBlank
+    @Min(value = 0)
+    @Digits(integer = 19, fraction = 2)
     private BigDecimal price;
+    @Size(max = 3000)
+    @PathToFile
     private String description;
     private String coverImage;
 }
