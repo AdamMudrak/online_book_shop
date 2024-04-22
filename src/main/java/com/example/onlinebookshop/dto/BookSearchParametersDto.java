404ed93ed426dd67afab177ec2bbc4055e47a601
AdamMudrak <mudrets2000@gmail.com>
2024-04-22 12:47:34 +0300

    Add needed, delete not needed annots

--- src/main/java/com/example/onlinebookshop/dto/BookSearchParametersDto.java
+++ src/main/java/com/example/onlinebookshop/dto/BookSearchParametersDto.java
@@ -1,18 +1,19 @@
 package com.example.onlinebookshop.dto;
 
+import com.example.onlinebookshop.validation.ValidPriceRange;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
+@ValidPriceRange
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record BookSearchParametersDto(
         @Min(0) @Digits(integer = 19, fraction = 2)
         BigDecimal fromPrice,
         @Min(1) @Digits(integer = 19, fraction = 2)
         BigDecimal toPrice,
-        @NotBlank String[] titles,
-        @NotBlank String[] authors
+        String[] titles,
+        String[] authors
 ) {
 }
