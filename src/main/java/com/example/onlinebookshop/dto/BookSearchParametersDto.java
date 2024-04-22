e62eadee6994b324f3b50eb96a01b6c9e38f979e
AdamMudrak <mudrets2000@gmail.com>
2024-04-22 03:26:11 +0300

    Add client level constraints in searching DTO

--- src/main/java/com/example/onlinebookshop/dto/BookSearchParametersDto.java
+++ src/main/java/com/example/onlinebookshop/dto/BookSearchParametersDto.java
@@ -1,8 +1,18 @@
 package com.example.onlinebookshop.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import java.math.BigDecimal;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record BookSearchParametersDto(String[] titles, String[] authors,
-                                      String fromPrice, String toPrice) {
+public record BookSearchParametersDto(
+        @Min(0) @Digits(integer = 19, fraction = 2)
+        BigDecimal fromPrice,
+        @Min(1) @Digits(integer = 19, fraction = 2)
+        BigDecimal toPrice,
+        @NotBlank String[] titles,
+        @NotBlank String[] authors
+) {
 }
