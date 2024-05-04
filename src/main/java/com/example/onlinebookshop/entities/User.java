b5b827c4b95a6a66dd41641cc4fb1743e0b8c558
AdamMudrak <mudrets2000@gmail.com>
2024-05-04 22:19:35 +0300

    Add User entity

---
+++ src/main/java/com/example/onlinebookshop/entities/User.java
@@ -0,0 +1,24 @@
+package com.example.onlinebookshop.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    private String shippingAddress;
+}
