34bbb71adbcf09da096fbdae672ae57d08936266
AdamMudrak <mudrets2000@gmail.com>
2024-05-05 14:49:44 +0300

    Add RegistrationException

---
+++ src/main/java/com/example/onlinebookshop/exceptions/RegistrationException.java
@@ -0,0 +1,7 @@
+package com.example.onlinebookshop.exceptions;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
