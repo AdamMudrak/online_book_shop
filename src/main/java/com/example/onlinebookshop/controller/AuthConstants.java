cf9ed2240f3ec3665c2c5e1d004dcb4ee156f610
AdamMudrak <mudrets2000@gmail.com>
2024-05-05 22:24:38 +0300

    Move constants to their own package, make them public

--- src/main/java/com/example/onlinebookshop/controller/AuthConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.onlinebookshop.controller;
-
-public class AuthConstants {
-    static final String AUTH_API_NAME = "Authentication API";
-    static final String AUTH_API_DESCRIPTION = "Here you'll find a comprehensive overview of how "
-            + "to register and login in  this app.";
-    static final String REGISTER_SUMMARY = "Register a new user in the app";
-}
---
+++ src/main/java/com/example/onlinebookshop/controller/constants/Constants.java
@@ -0,0 +1,19 @@
+package com.example.onlinebookshop.controller.constants;
+
+public class Constants {
+    public static final String SUCCESSFULLY_RETRIEVED = "Successfully retrieved";
+    public static final String SUCCESSFULLY_CREATED = "Successfully created";
+    public static final String SUCCESSFULLY_UPDATED = "Successfully updated";
+    public static final String SUCCESSFULLY_REGISTERED = "Successfully registered";
+    public static final String INVALID_ID_DESCRIPTION = "Either a negative or non-existing id "
+            + "provided. API will tell which one";
+    public static final String INVALID_ENTITY_VALUE =
+            "One of the parameters is invalid. API will show which one";
+    public static final String CODE_200 = "200";
+    public static final String CODE_201 = "201";
+    public static final String CODE_204 = "204";
+    public static final String CODE_204_DESCRIPTION = "No content";
+    public static final String CODE_400 = "400";
+    public static final String ID = "id";
+    public static final String ID_EXAMPLE = "1";
+}
