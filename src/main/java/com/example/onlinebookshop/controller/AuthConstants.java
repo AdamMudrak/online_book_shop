b4ded6e45b186e66f569a15710b6dbbda1d3a139
AdamMudrak <mudrets2000@gmail.com>
2024-05-05 15:32:52 +0300

    Add Auth specific constants

---
+++ src/main/java/com/example/onlinebookshop/controller/AuthConstants.java
@@ -0,0 +1,8 @@
+package com.example.onlinebookshop.controller;
+
+public class AuthConstants {
+    static final String AUTH_API_NAME = "Authentication API";
+    static final String AUTH_API_DESCRIPTION = "Here you'll find a comprehensive overview of how "
+            + "to register and login in  this app.";
+    static final String REGISTER_SUMMARY = "Register a new user in the app";
+}
