73b053e70627b1246da8c36d046b8e03000bffe5
AdamMudrak <mudrets2000@gmail.com>
2024-05-05 15:33:07 +0300

    Add AuthController

---
+++ src/main/java/com/example/onlinebookshop/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.onlinebookshop.controller;
+
+import com.example.onlinebookshop.dto.user.UserRegistrationRequestDto;
+import com.example.onlinebookshop.dto.user.UserResponseDto;
+import com.example.onlinebookshop.exceptions.RegistrationException;
+import com.example.onlinebookshop.services.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@Tag(name = AuthConstants.AUTH_API_NAME,
+        description = AuthConstants.AUTH_API_DESCRIPTION)
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserService userService;
+
+    @Operation(summary = AuthConstants.REGISTER_SUMMARY)
+    @ApiResponse(responseCode = Constants.CODE_200, description = Constants.SUCCESSFULLY_REGISTERED)
+    @PostMapping("/registration")
+    public UserResponseDto registerUser(@RequestBody @Valid UserRegistrationRequestDto requestDto)
+            throws RegistrationException {
+        return userService.register(requestDto);
+    }
+}
