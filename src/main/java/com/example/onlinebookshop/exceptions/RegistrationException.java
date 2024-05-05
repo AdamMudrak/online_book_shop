b74b6fe3bd8467212802454bbd51d09403074f36
AdamMudrak <mudrets2000@gmail.com>
2024-05-05 21:08:26 +0300

    Make your RegistrationException checked

--- src/main/java/com/example/onlinebookshop/exceptions/RegistrationException.java
+++ src/main/java/com/example/onlinebookshop/exceptions/RegistrationException.java
@@ -1,6 +1,6 @@
 package com.example.onlinebookshop.exceptions;
 
-public class RegistrationException extends RuntimeException {
+public class RegistrationException extends Exception {
     public RegistrationException(String message) {
         super(message);
     }
