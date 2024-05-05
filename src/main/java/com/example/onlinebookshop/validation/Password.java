66bfa125411267f7a78f73434add418a0621a1dc
AdamMudrak <mudrets2000@gmail.com>
2024-05-05 21:58:16 +0300

    Change message. Postman doesn't natively support text blocks

--- src/main/java/com/example/onlinebookshop/validation/Password.java
+++ src/main/java/com/example/onlinebookshop/validation/Password.java
@@ -11,15 +11,12 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Password {
-    String message() default """
-                    Invalid password
-                    
-                    Your password should contain:
-                    1) at least one lowercase letter;
-                    2) at least one uppercase letter;
-                    3) at least one number;
-                    4) at least one special character;
-                    5) from 8 to 32 characters.""";
+    String message() default "Password should contain"
+            + " 1 lowercase letter,"
+            + " 1 uppercase letter,"
+            + " 1 digit,"
+            + " 1 special character,"
+            + " and be 8 to 32 characters long";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
