faab897f6cb133a2ceddc1dea8106492fbf79a1e
AdamMudrak <mudrets2000@gmail.com>
2024-05-06 11:04:21 +0300

    Rebuild response DTOs as records

--- src/main/java/com/example/onlinebookshop/dto/book/BookDto.java
+++ src/main/java/com/example/onlinebookshop/dto/book/BookDto.java
@@ -1,29 +1,11 @@
 package com.example.onlinebookshop.dto.book;
 
-import com.example.onlinebookshop.constants.BookDtoConstants;
-import com.example.onlinebookshop.constants.Constants;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Min;
 import java.math.BigDecimal;
-import lombok.Data;
 
-@Data
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class BookDto {
-    @Schema(name = Constants.ID, example = Constants.ID_EXAMPLE)
-    private Long id;
-    @Schema(name = BookDtoConstants.TITLE, example = BookDtoConstants.TITLE_EXAMPLE)
-    private String title;
-    @Schema(name = BookDtoConstants.AUTHOR, example = BookDtoConstants.AUTHOR_EXAMPLE)
-    private String author;
-    @Schema(name = BookDtoConstants.ISBN, example = BookDtoConstants.ISBN_EXAMPLE)
-    private String isbn;
-    @Schema(name = BookDtoConstants.PRICE, example = BookDtoConstants.PRICE_EXAMPLE)
-    @Min(value = 0)
-    private BigDecimal price;
-    @Schema(name = BookDtoConstants.DESCRIPTION, example = BookDtoConstants.DESCRIPTION_EXAMPLE)
-    private String description;
-    @Schema(name = BookDtoConstants.COVER_IMAGE, example = BookDtoConstants.COVER_IMAGE_EXAMPLE)
-    private String coverImage;
-}
+public record BookDto(Long id,
+                      String title,
+                      String author,
+                      String isbn,
+                      BigDecimal price,
+                      String description,
+                      String coverImage){}
--- src/main/java/com/example/onlinebookshop/dto/user/UserResponseDto.java
+++ src/main/java/com/example/onlinebookshop/dto/user/UserResponseDto.java
@@ -1,23 +1,7 @@
 package com.example.onlinebookshop.dto.user;
 
-import com.example.onlinebookshop.constants.Constants;
-import com.example.onlinebookshop.constants.UserDtoConstants;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
-
-@Data
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class UserResponseDto {
-    @Schema(name = Constants.ID, example = Constants.ID_EXAMPLE)
-    private Long id;
-    @Schema(name = UserDtoConstants.EMAIL, example = UserDtoConstants.EMAIL_EXAMPLE)
-    private String email;
-    @Schema(name = UserDtoConstants.FIRST_NAME, example = UserDtoConstants.FIRST_NAME_EXAMPLE)
-    private String firstName;
-    @Schema(name = UserDtoConstants.LAST_NAME, example = UserDtoConstants.LAST_NAME_EXAMPLE)
-    private String lastName;
-    @Schema(name = UserDtoConstants.SHIPPING_ADDRESS,
-            example = UserDtoConstants.SHIPPING_ADDRESS_EXAMPLE)
-    private String shippingAddress;
-}
+public record UserResponseDto(Long id,
+                              String email,
+                              String firstName,
+                              String lastName,
+                              String shippingAddress){}
