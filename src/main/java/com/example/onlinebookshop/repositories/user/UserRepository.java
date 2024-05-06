22a07856b62e5d040a32bc1889cc5e0845100edb
AdamMudrak <mudrets2000@gmail.com>
2024-05-06 09:41:46 +0300

    update repo according to recommendation

--- src/main/java/com/example/onlinebookshop/repositories/user/UserRepository.java
+++ src/main/java/com/example/onlinebookshop/repositories/user/UserRepository.java
@@ -1,9 +1,8 @@
 package com.example.onlinebookshop.repositories.user;
 
 import com.example.onlinebookshop.entities.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
