f86a9600576e6bb05fedf8baf4ac769a2a33e742
AdamMudrak <mudrets2000@gmail.com>
2024-05-06 11:05:21 +0300

    Rebuild SearchDto as class - seems to be more readable with all these annotations

--- src/main/java/com/example/onlinebookshop/dto/book/BookSearchParametersDto.java
+++ src/main/java/com/example/onlinebookshop/dto/book/BookSearchParametersDto.java
@@ -7,23 +7,26 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
+import lombok.Data;
 
+@Data
 @ValidPriceRange
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record BookSearchParametersDto(
-        @Schema(example = BookDtoConstants.FLOOR_PRICE,
-                description = BookDtoConstants.FLOOR_PRICE_DESCRIPTION)
-        @Positive @Digits(integer = 17, fraction = 2)
-        BigDecimal fromPrice,
-        @Schema(example = BookDtoConstants.CEILING_PRICE,
-                description = BookDtoConstants.CEILING_PRICE_DESCRIPTION)
-        @Positive @Digits(integer = 17, fraction = 2)
-        BigDecimal toPrice,
-        @Schema(example = BookDtoConstants.TITLE_EXAMPLE,
-                description = BookDtoConstants.TITLE_DESCRIPTION)
-        String[] titles,
-        @Schema(example = BookDtoConstants.AUTHOR_EXAMPLE,
-                description = BookDtoConstants.AUTHOR_DESCRIPTION)
-        String[] authors
-) {
+public class BookSearchParametersDto {
+    @Schema(example = BookDtoConstants.FLOOR_PRICE,
+            description = BookDtoConstants.FLOOR_PRICE_DESCRIPTION)
+    @Positive
+    @Digits(integer = 17, fraction = 2)
+    private BigDecimal fromPrice;
+    @Schema(example = BookDtoConstants.CEILING_PRICE,
+            description = BookDtoConstants.CEILING_PRICE_DESCRIPTION)
+    @Positive
+    @Digits(integer = 17, fraction = 2)
+    private BigDecimal toPrice;
+    @Schema(example = BookDtoConstants.TITLE_EXAMPLE,
+            description = BookDtoConstants.TITLE_DESCRIPTION)
+    private String[] titles;
+    @Schema(example = BookDtoConstants.AUTHOR_EXAMPLE,
+            description = BookDtoConstants.AUTHOR_DESCRIPTION)
+    private String[] authors;
 }
