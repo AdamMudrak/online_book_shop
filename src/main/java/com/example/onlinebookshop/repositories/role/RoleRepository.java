823ae582e150797b5d81c65f502806b3ec5ab0dd
AdamMudrak <mudrets2000@gmail.com>
2024-05-07 23:39:30 +0300

    Add role repo

---
+++ src/main/java/com/example/onlinebookshop/repositories/role/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.onlinebookshop.repositories.role;
+
+import com.example.onlinebookshop.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
