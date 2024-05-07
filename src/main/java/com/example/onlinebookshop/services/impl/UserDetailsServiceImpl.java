d22b05fe512b334408a61db41a0e4b6fc73dfa6d
AdamMudrak <mudrets2000@gmail.com>
2024-05-07 23:22:18 +0300

    Implement UserDetailsService

---
+++ src/main/java/com/example/onlinebookshop/services/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.onlinebookshop.services.impl;
+
+import com.example.onlinebookshop.repositories.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(
+                "Can't find user by email " + email));
+    }
+}
