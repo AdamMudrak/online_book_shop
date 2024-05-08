3c0484fde33824bc6c75880d0f2170bb18160860
AdamMudrak <mudrets2000@gmail.com>
2024-05-08 23:28:18 +0300

    Escape LIE the easy way

--- src/main/java/com/example/onlinebookshop/entities/User.java
+++ src/main/java/com/example/onlinebookshop/entities/User.java
@@ -2,6 +2,7 @@ package com.example.onlinebookshop.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,7 +35,7 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String lastName;
     private String shippingAddress;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
