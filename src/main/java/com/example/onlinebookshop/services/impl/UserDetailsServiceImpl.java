4ebd8e0a7aaba82ac6172765729b3b577aa5d05c
AdamMudrak <mudrets2000@gmail.com>
2024-05-09 11:59:56 +0300

    Delete an unneeded, add needed import

--- src/main/java/com/example/onlinebookshop/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/example/onlinebookshop/services/impl/UserDetailsServiceImpl.java
@@ -1,10 +1,10 @@
 package com.example.onlinebookshop.services.impl;
 
+import com.example.onlinebookshop.exceptions.EntityNotFoundException;
 import com.example.onlinebookshop.repositories.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
