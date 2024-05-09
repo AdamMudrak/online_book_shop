5f5e1591b44d83a28dd596f6cdb45648da7b3e32
Adam Mudrak <113807446+AdamMudrak@users.noreply.github.com>
2024-05-09 11:44:49 +0300

    Update src/main/java/com/example/onlinebookshop/services/impl/UserDetailsServiceImpl.java
    Co-authored-by: Olena Bruiako <bruyako.elena@gmail.com>

--- src/main/java/com/example/onlinebookshop/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/example/onlinebookshop/services/impl/UserDetailsServiceImpl.java
@@ -13,7 +13,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) {
         return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(
                 "Can't find user by email " + email));
     }
