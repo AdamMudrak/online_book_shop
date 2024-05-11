80bdd02dede0c4c2e8c646bacc0f16e4034d5431
AdamMudrak <mudrets2000@gmail.com>
2024-05-12 02:36:52 +0300

    Line longer than 100 - fix

--- src/main/java/com/example/onlinebookshop/constants/AuthConstants.java
+++ src/main/java/com/example/onlinebookshop/constants/AuthConstants.java
@@ -5,5 +5,6 @@ public class AuthConstants {
     public static final String AUTH_API_DESCRIPTION =
             "Here you'll find a comprehensive overview of how to register and login in  this app.";
     public static final String REGISTER_SUMMARY = "Register a new user in the app";
-    public static final String AUTH_EXCEPTION_MESSAGE = "Can't authenticate user: wrong username or password";
+    public static final String AUTH_EXCEPTION_MESSAGE =
+            "Can't authenticate user: wrong username or password";
 }
