a41a45f938c4ff145d380a91a251585f2bfcedbe
AdamMudrak <mudrets2000@gmail.com>
2024-05-12 01:03:34 +0300

    Add authenticate method and its impl

--- src/main/java/com/example/onlinebookshop/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/onlinebookshop/services/impl/UserServiceImpl.java
@@ -1,13 +1,21 @@
 package com.example.onlinebookshop.services.impl;
 
+import static com.example.onlinebookshop.constants.AuthConstants.AUTH_EXCEPTION_MESSAGE;
+
+import com.example.onlinebookshop.dto.user.UserLoginRequestDto;
+import com.example.onlinebookshop.dto.user.UserLoginResponseDto;
 import com.example.onlinebookshop.dto.user.UserRegistrationRequestDto;
 import com.example.onlinebookshop.dto.user.UserResponseDto;
 import com.example.onlinebookshop.entities.User;
+import com.example.onlinebookshop.exceptions.AuthenticationException;
 import com.example.onlinebookshop.exceptions.RegistrationException;
 import com.example.onlinebookshop.mapper.UserMapper;
 import com.example.onlinebookshop.repositories.user.UserRepository;
+import com.example.onlinebookshop.security.JwtUtil;
 import com.example.onlinebookshop.services.UserService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +23,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -23,7 +33,23 @@ public class UserServiceImpl implements UserService {
             throw new RegistrationException("User with email "
                     + requestDto.getEmail() + " already exists");
         }
-        User savedUser = userRepository.save(userMapper.toUser(requestDto));
+        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        User userWithEncodedPassword = userMapper.toUser(requestDto);
+        User savedUser = userRepository.save(userWithEncodedPassword);
         return userMapper.toUserResponseDto(savedUser);
+    }
+
+    @Override
+    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
+        Optional<User> user = userRepository.findByEmail(requestDto.email());
+        if (user.isEmpty()) {
+            throw new AuthenticationException(AUTH_EXCEPTION_MESSAGE);
+        }
+        String rawPassword = requestDto.password();
+        String userPasswordFromDb = user.get().getPassword();
+        if (!passwordEncoder.matches(rawPassword, userPasswordFromDb)) {
+            throw new AuthenticationException(AUTH_EXCEPTION_MESSAGE);
+        }
+        return new UserLoginResponseDto(jwtUtil.generateToken(requestDto.email()));
     }
 }
