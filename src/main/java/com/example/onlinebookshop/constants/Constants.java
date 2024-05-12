89f6a4b01f6fc3d2503bdea1830dbf916a25f302
AdamMudrak <mudrets2000@gmail.com>
2024-05-13 00:08:44 +0300

    Fix mistaken message

--- src/main/java/com/example/onlinebookshop/constants/Constants.java
+++ src/main/java/com/example/onlinebookshop/constants/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String SUCCESSFULLY_CREATED = "Successfully created";
     public static final String SUCCESSFULLY_UPDATED = "Successfully updated";
     public static final String SUCCESSFULLY_REGISTERED = "Successfully registered";
-    public static final String SUCCESSFULLY_LOGGED_IN = "Successfully registered";
+    public static final String SUCCESSFULLY_LOGGED_IN = "Successfully logged in";
     public static final String INVALID_ID_DESCRIPTION =
             "Either a negative or non-existing id provided. API will tell which one";
     public static final String INVALID_ENTITY_VALUE =
