8ac45eb6eb1553642a986c13670f17cb2998804d
AdamMudrak <mudrets2000@gmail.com>
2024-05-13 00:17:59 +0300

    Add Getter and Setter to Role entity

--- src/main/java/com/example/onlinebookshop/entities/Role.java
+++ src/main/java/com/example/onlinebookshop/entities/Role.java
@@ -8,8 +8,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
