1509074082a00024803e39b5d7bf587ddc83eb81
AdamMudrak <mudrets2000@gmail.com>
2024-05-13 00:12:44 +0300

    Delete an unneeded method

--- src/main/java/com/example/onlinebookshop/services/UserService.java
+++ src/main/java/com/example/onlinebookshop/services/UserService.java
@@ -1,13 +1,9 @@
 package com.example.onlinebookshop.services;
 
-import com.example.onlinebookshop.dto.user.UserLoginRequestDto;
-import com.example.onlinebookshop.dto.user.UserLoginResponseDto;
 import com.example.onlinebookshop.dto.user.UserRegistrationRequestDto;
 import com.example.onlinebookshop.dto.user.UserResponseDto;
 import com.example.onlinebookshop.exceptions.RegistrationException;
 
 public interface UserService {
     UserResponseDto register(UserRegistrationRequestDto requestDto) throws RegistrationException;
-
-    UserLoginResponseDto authenticate(UserLoginRequestDto requestDto);
 }
