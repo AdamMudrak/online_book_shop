99e26ff37ea6721e34db4281fc9a191a17189e53
AdamMudrak <mudrets2000@gmail.com>
2024-05-14 13:38:54 +0300

    Add findByCategory

--- src/main/java/com/example/onlinebookshop/repositories/book/BookRepository.java
+++ src/main/java/com/example/onlinebookshop/repositories/book/BookRepository.java
@@ -1,9 +1,10 @@
 package com.example.onlinebookshop.repositories.book;
 
 import com.example.onlinebookshop.entities.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-
+    List<Book> findAllByCategoryId(Long categoryId);
 }
