bf10a792e82d7e59079f3b5c1da952fed6df4433
AdamMudrak <mudrets2000@gmail.com>
2024-05-14 13:52:52 +0300

    Make BookDto have categories

--- src/main/java/com/example/onlinebookshop/dto/book/BookDto.java
+++ src/main/java/com/example/onlinebookshop/dto/book/BookDto.java
@@ -1,10 +1,13 @@
 package com.example.onlinebookshop.dto.book;
 
+import com.example.onlinebookshop.dto.category.CategoryDto;
 import java.math.BigDecimal;
+import java.util.Set;
 
 public record BookDto(Long id,
                       String title,
                       String author,
+                      Set<CategoryDto> categories,
                       String isbn,
                       BigDecimal price,
                       String description,
