ea807a2c485fcc48fde2b84f367d36f4ff28a467
AdamMudrak <mudrets2000@gmail.com>
2024-05-14 13:53:10 +0300

    Add BookDtoWithoutCategoryIds

---
+++ src/main/java/com/example/onlinebookshop/dto/book/BookDtoWithoutCategoryIds.java
@@ -0,0 +1,12 @@
+package com.example.onlinebookshop.dto.book;
+
+import java.math.BigDecimal;
+
+public record BookDtoWithoutCategoryIds(Long id,
+                                        String title,
+                                        String author,
+                                        String isbn,
+                                        BigDecimal price,
+                                        String description,
+                                        String coverImage) {
+}
