1e89a0b8fd7af4769fb43e7b3b3bfbd4db439cee
AdamMudrak <mudrets2000@gmail.com>
2024-05-15 00:19:16 +0300

    Add CategoryController

--- src/main/java/com/example/onlinebookshop/controller/CategoryController.java
+++ src/main/java/com/example/onlinebookshop/controller/CategoryController.java
@@ -1,14 +1,37 @@
 package com.example.onlinebookshop.controller;
 
+import com.example.onlinebookshop.constants.BookConstants;
 import com.example.onlinebookshop.constants.CategoryConstants;
+import com.example.onlinebookshop.constants.Constants;
+import com.example.onlinebookshop.dto.book.BookDtoWithoutCategoryIds;
 import com.example.onlinebookshop.dto.category.CategoryDto;
+import com.example.onlinebookshop.dto.category.UpdateCategoryDto;
+import com.example.onlinebookshop.services.BookService;
 import com.example.onlinebookshop.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RequiredArgsConstructor
 @RestController
 @Tag(name = CategoryConstants.CATEGORY_API_NAME,
@@ -16,29 +39,89 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/categories")
 public class CategoryController {
     private final CategoryService categoryService;
+    private final BookService bookService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = CategoryConstants.GET_ALL_SUMMARY,
+            description = CategoryConstants.GET_ALL_DESCRIPTION)
+    @ApiResponse(responseCode = Constants.CODE_200, description = Constants.SUCCESSFULLY_RETRIEVED)
+    @GetMapping
+    public List<CategoryDto> getAll(
+            @Parameter(example = CategoryConstants.PAGEABLE_EXAMPLE) Pageable pageable) {
+        return categoryService.findAll(pageable);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = CategoryConstants.GET_BY_ID_SUMMARY,
+            description = CategoryConstants.GET_BY_ID_DESCRIPTION)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = Constants.CODE_200,
+                    description = Constants.SUCCESSFULLY_RETRIEVED),
+            @ApiResponse(responseCode = Constants.CODE_400,
+                    description = Constants.INVALID_ID_DESCRIPTION)
+    })
+    @GetMapping("/{id}")
+    public CategoryDto getCategoryById(@PathVariable @Parameter(name = Constants.ID,
+            description = CategoryConstants.VALID_ID_DESCRIPTION,
+            example = Constants.ID_EXAMPLE) @Positive Long id) {
+        return categoryService.getById(id);
+    }
+
+    //TODO ???
+    @GetMapping("/{id}/books")
+    public List<BookDtoWithoutCategoryIds> getBooksByCategoryId(@PathVariable @Parameter(name = Constants.ID,
+            description = BookConstants.VALID_ID_DESCRIPTION,
+            example = Constants.ID_EXAMPLE) @Positive Long id) {
+        return bookService.findAllWithoutCategoryIds(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Operation(summary = CategoryConstants.CREATE_CATEGORY_SUMMARY,
+            description = CategoryConstants.CREATE_CATEGORY_DESCRIPTION)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = Constants.CODE_201,
+                    description = Constants.SUCCESSFULLY_CREATED),
+            @ApiResponse(responseCode = Constants.CODE_400,
+                    description = Constants.INVALID_ENTITY_VALUE)
+    })
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public CategoryDto createCategory(CategoryDto categoryDto) {
-        return null;
+        return categoryService.save(categoryDto);
     }
 
-    public List getAll() {
-        return null;
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Operation(summary = CategoryConstants.UPDATE_CATEGORY_SUMMARY,
+            description = CategoryConstants.UPDATE_CATEGORY_DESCRIPTION)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = Constants.CODE_200,
+                    description = Constants.SUCCESSFULLY_UPDATED),
+            @ApiResponse(responseCode = Constants.CODE_400,
+                    description = Constants.INVALID_ID_DESCRIPTION
+                            + ". Or " + Constants.INVALID_ENTITY_VALUE)
+    })
+    @PutMapping("/{id}")
+    public CategoryDto updateCategory(@RequestBody @Valid UpdateCategoryDto categoryDto,
+                                      @PathVariable @Parameter(name = Constants.ID,
+                                              description = CategoryConstants.VALID_ID_DESCRIPTION,
+                                              example = Constants.ID_EXAMPLE) @Positive Long id) {
+        return categoryService.update(categoryDto, id);
     }
 
-    public CategoryDto getCategoryById(Long id) {
-        return null;
-    }
-
-    public CategoryDto updateCategory(Long id, CategoryDto categoryDto) {
-        return null;
-    }
-
-    public void deleteCategory(Long id) {
-
-    }
-
-    //(endpoint: "/{id}/books")
-    public List getBooksByCategoryId(Long id)  {
-        return null;
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Operation(summary = CategoryConstants.DELETE_CATEGORY_SUMMARY,
+            description = CategoryConstants.DELETE_CATEGORY_DESCRIPTION)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = Constants.CODE_204,
+                    description = Constants.CODE_204_DESCRIPTION),
+            @ApiResponse(responseCode = Constants.CODE_400,
+                    description = Constants.INVALID_ID_DESCRIPTION)
+    })
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable @Parameter(name = Constants.ID,
+            description = BookConstants.VALID_ID_DESCRIPTION,
+            example = Constants.ID_EXAMPLE) @Positive Long id) {
+        categoryService.deleteById(id);
     }
 }
