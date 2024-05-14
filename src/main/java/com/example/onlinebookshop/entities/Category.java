d4e26ee71675e6b4c592345875908d89abdcbec8
AdamMudrak <mudrets2000@gmail.com>
2024-05-14 13:24:39 +0300

    Add entity for Category

---
+++ src/main/java/com/example/onlinebookshop/entities/Category.java
@@ -0,0 +1,23 @@
+package com.example.onlinebookshop.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    private String description;
+}
