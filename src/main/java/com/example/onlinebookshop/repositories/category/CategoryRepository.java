9be0a5e8750e230f049c9eebdb783ce3248569f5
AdamMudrak <mudrets2000@gmail.com>
2024-05-16 02:40:46 +0300

    Add according function for looking for category name

--- src/main/java/com/example/onlinebookshop/repositories/category/CategoryRepository.java
+++ src/main/java/com/example/onlinebookshop/repositories/category/CategoryRepository.java
@@ -1,6 +1,9 @@
 package com.example.onlinebookshop.repositories.category;
 
 import com.example.onlinebookshop.entities.Category;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {}
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
