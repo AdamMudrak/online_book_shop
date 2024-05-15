05e97923f384263b31eb4cbaeb5f1202f97cff4b
AdamMudrak <mudrets2000@gmail.com>
2024-05-15 14:25:29 +0300

    Use @Query for BookRepo

--- src/main/java/com/example/onlinebookshop/repositories/book/BookRepository.java
+++ src/main/java/com/example/onlinebookshop/repositories/book/BookRepository.java
@@ -4,7 +4,11 @@ import com.example.onlinebookshop.entities.Book;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @Query(value = "SELECT book FROM Book book "
+            + "INNER JOIN book.categories categories "
+            + "WHERE categories.id = :categoryId")
     List<Book> findAllByCategoryId(Long categoryId);
 }
