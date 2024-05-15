80b4bf1038cfc9d89792d0a2a0f12b348a7fb2f5
AdamMudrak <mudrets2000@gmail.com>
2024-05-15 18:05:15 +0300

    Delete an unneeded exception

--- src/main/java/com/example/onlinebookshop/exceptions/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.onlinebookshop.exceptions;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
