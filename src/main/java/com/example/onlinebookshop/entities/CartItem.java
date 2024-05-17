ef8ccebf6d469380dc97e8451baa208502c28a41
AdamMudrak <mudrets2000@gmail.com>
2024-05-17 16:46:46 +0300

    Add needed relations

--- src/main/java/com/example/onlinebookshop/entities/CartItem.java
+++ src/main/java/com/example/onlinebookshop/entities/CartItem.java
@@ -2,9 +2,13 @@ package com.example.onlinebookshop.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,8 +22,15 @@ public class CartItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shopping_cart_id")
     private ShoppingCart shoppingCart;
     @Column(nullable = false)
+    @ManyToOne
+    @JoinTable(
+            name = "cart_items_books",
+            joinColumns = @JoinColumn(name = "cart_item_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id"))
     private Book book;
     @Column(nullable = false)
     private int quantity;
--- src/main/java/com/example/onlinebookshop/entities/ShoppingCart.java
+++ src/main/java/com/example/onlinebookshop/entities/ShoppingCart.java
@@ -2,9 +2,13 @@ package com.example.onlinebookshop.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,7 +28,10 @@ public class ShoppingCart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "shoppingCart")
     private Set<CartItem> cartItems = new HashSet<>();
     @Column(nullable = false)
     private boolean isDeleted = false;
