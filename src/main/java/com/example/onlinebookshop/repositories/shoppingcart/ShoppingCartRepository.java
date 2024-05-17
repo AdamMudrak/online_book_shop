c86b2cad6aadc1c137fc57b725fbf66e78b7d140
AdamMudrak <mudrets2000@gmail.com>
2024-05-18 01:52:27 +0300

    Add ShoppingCartRepo

--- src/main/java/com/example/onlinebookshop/repositories/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/com/example/onlinebookshop/repositories/shoppingcart/ShoppingCartRepository.java
@@ -2,6 +2,11 @@ package com.example.onlinebookshop.repositories.shoppingcart;
 
 import com.example.onlinebookshop.entities.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+    @Query(value = "SELECT cart FROM ShoppingCart cart "
+            + "INNER JOIN cart.user user "
+            + "WHERE user.email = :email")
+    ShoppingCart findByUserEmail(String email);
 }
