e90df0ca8e902faa836b9c1f801ea01dcfa23f04
AdamMudrak <mudrets2000@gmail.com>
2024-05-27 15:46:26 +0300

    Make CartItemDto a class

--- src/main/java/com/example/onlinebookshop/dto/cartitem/response/CartItemDto.java
+++ src/main/java/com/example/onlinebookshop/dto/cartitem/response/CartItemDto.java
@@ -1,8 +1,13 @@
 package com.example.onlinebookshop.dto.cartitem.response;
 
-public record CartItemDto(
-        Long outerId,
-        Long bookId,
-        String bookTitle,
-        int quantity) {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartItemDto {
+    private Long outerId;
+    private Long bookId;
+    private String bookTitle;
+    private int quantity;
 }
