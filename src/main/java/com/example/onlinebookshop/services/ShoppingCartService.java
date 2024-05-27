d0b438ac626eefd383937c4edb9f1214d910ec0b
AdamMudrak <mudrets2000@gmail.com>
2024-05-27 13:17:42 +0300

    Use user id instead of DTOs

--- src/main/java/com/example/onlinebookshop/services/ShoppingCartService.java
+++ src/main/java/com/example/onlinebookshop/services/ShoppingCartService.java
@@ -3,16 +3,15 @@ package com.example.onlinebookshop.services;
 import com.example.onlinebookshop.dto.cartitem.request.AddCartItemDto;
 import com.example.onlinebookshop.dto.shoppingcart.request.UpdateQuantityInShoppingCartDto;
 import com.example.onlinebookshop.dto.shoppingcart.response.ShoppingCartDto;
-import com.example.onlinebookshop.dto.user.request.SendUserTokenRequestDto;
 
 public interface ShoppingCartService {
-    ShoppingCartDto getShoppingCartByUserId(SendUserTokenRequestDto token);
+    ShoppingCartDto getShoppingCartByUserId(Long userId);
 
-    ShoppingCartDto addBookToShoppingCart(SendUserTokenRequestDto token,
+    ShoppingCartDto addBookToShoppingCart(Long userId,
                                           AddCartItemDto addCartItemDto);
 
-    ShoppingCartDto updateBookQuantity(SendUserTokenRequestDto token, Long cartItemId,
+    ShoppingCartDto updateBookQuantity(Long userId, Long cartItemId,
                                        UpdateQuantityInShoppingCartDto quantity);
 
-    void deleteBookFromShoppingCart(SendUserTokenRequestDto token, Long cartItemId);
+    void deleteBookFromShoppingCart(Long userId, Long cartItemId);
 }
