741c0f1ff312bbac96519d5836a5d167d51aa33c
AdamMudrak <mudrets2000@gmail.com>
2024-05-27 13:18:10 +0300

    Use after mapping for setting userId

--- src/main/java/com/example/onlinebookshop/mapper/ShoppingCartMapper.java
+++ src/main/java/com/example/onlinebookshop/mapper/ShoppingCartMapper.java
@@ -3,9 +3,19 @@ package com.example.onlinebookshop.mapper;
 import com.example.onlinebookshop.config.MapperConfig;
 import com.example.onlinebookshop.dto.shoppingcart.response.ShoppingCartDto;
 import com.example.onlinebookshop.entities.ShoppingCart;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface ShoppingCartMapper {
+    @Mapping(target = "userId", ignore = true)
     ShoppingCartDto toShoppingCartDto(ShoppingCart shoppingCart);
+
+    @AfterMapping
+    default void setUserId(@MappingTarget ShoppingCartDto shoppingCartDto,
+                           ShoppingCart shoppingCart) {
+        Long userId = shoppingCart.getUser().getId();
+    }
 }
