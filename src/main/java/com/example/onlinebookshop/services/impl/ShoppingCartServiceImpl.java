316a164e2da5513d4466d0c4fa52b5529e8cf275
AdamMudrak <mudrets2000@gmail.com>
2024-05-27 22:34:07 +0300

    Everything works in service now

---
+++ src/main/java/com/example/onlinebookshop/services/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,102 @@
+package com.example.onlinebookshop.services.impl;
+
+import com.example.onlinebookshop.dto.cartitem.request.AddCartItemDto;
+import com.example.onlinebookshop.dto.shoppingcart.request.UpdateQuantityInShoppingCartDto;
+import com.example.onlinebookshop.dto.shoppingcart.response.ShoppingCartDto;
+import com.example.onlinebookshop.entities.CartItem;
+import com.example.onlinebookshop.entities.ShoppingCart;
+import com.example.onlinebookshop.exceptions.EntityNotFoundException;
+import com.example.onlinebookshop.mapper.CartItemMapper;
+import com.example.onlinebookshop.mapper.ShoppingCartMapper;
+import com.example.onlinebookshop.repositories.book.BookRepository;
+import com.example.onlinebookshop.repositories.cartitem.CartItemRepository;
+import com.example.onlinebookshop.repositories.shoppingcart.ShoppingCartRepository;
+import com.example.onlinebookshop.services.ShoppingCartService;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+//todo используй пагинацию, сортинг, сваггер на уровне контроллера
+
+@Service
+@RequiredArgsConstructor
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    private final ShoppingCartRepository shoppingCartRepository;
+    private final CartItemRepository cartItemRepository;
+    private final CartItemMapper cartItemMapper;
+    private final ShoppingCartMapper shoppingCartMapper;
+    private final BookRepository bookRepository;
+
+    @Override
+    public ShoppingCartDto getShoppingCartByUserId(Long userId) {
+        ShoppingCart shoppingCart = shoppingCartRepository.findByUserId(userId);
+        ShoppingCartDto shoppingCartDto =
+                shoppingCartMapper.toShoppingCartDto(shoppingCartRepository.findByUserId(userId));
+        setCartItemsInDto(shoppingCartDto, shoppingCart);
+        return shoppingCartDto;
+    }
+
+    @Override
+    public ShoppingCartDto addBookToShoppingCart(Long userId,
+                                                 AddCartItemDto addCartItemDto) {
+        ShoppingCart shoppingCart = shoppingCartRepository.findByUserId(userId);
+
+        CartItem cartItem = new CartItem();
+        cartItem.setShoppingCart(shoppingCart);
+        Long bookId = addCartItemDto.bookId();
+        cartItem.setBook(bookRepository.findById(bookId).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book by id " + bookId)));
+        cartItem.setQuantity(addCartItemDto.quantity());
+        setOuterId(shoppingCart, cartItem);
+        cartItemRepository.save(cartItem);
+        shoppingCart.getCartItems().add(cartItem);
+        ShoppingCartDto shoppingCartDto = shoppingCartMapper.toShoppingCartDto(shoppingCart);
+        setCartItemsInDto(shoppingCartDto, shoppingCart);
+        return shoppingCartDto;
+    }
+
+    @Override
+    public ShoppingCartDto updateBookQuantity(Long userId, Long cartItemId,
+                                              UpdateQuantityInShoppingCartDto quantity) {
+        ShoppingCart shoppingCart = shoppingCartRepository.findByUserId(userId);
+        CartItem cartItem = getCartItemById(shoppingCart, cartItemId);
+        cartItem.setQuantity(quantity.quantity());
+        ShoppingCartDto shoppingCartDto = shoppingCartMapper.toShoppingCartDto(shoppingCart);
+        setCartItemsInDto(shoppingCartDto, shoppingCart);
+        return shoppingCartDto;
+    }
+
+    @Override
+    public void deleteBookFromShoppingCart(Long userId, Long cartItemId) {
+        ShoppingCart shoppingCart = shoppingCartRepository.findByUserId(userId);
+        CartItem cartItem = getCartItemById(shoppingCart, cartItemId);
+        shoppingCart.getCartItems().remove(cartItem);
+        cartItemRepository.delete(cartItem);
+        shoppingCartRepository.save(shoppingCart);
+    }
+
+    private CartItem getCartItemById(ShoppingCart shoppingCart, Long cartItemId) {
+        return shoppingCart.getCartItems().stream()
+                .filter(cartItemInCart -> cartItemInCart.getOuterId().equals(cartItemId))
+                .findFirst().orElseThrow(() -> new EntityNotFoundException(
+                        "CartItem with id " + cartItemId + " not found"));
+    }
+
+    private void setCartItemsInDto(ShoppingCartDto shoppingCartDto, ShoppingCart shoppingCart) {
+        shoppingCartDto.setCartItems(shoppingCart.getCartItems()
+                .stream()
+                .map(cartItemMapper::toCartItemDto)
+                .collect(Collectors.toSet()));
+    }
+
+    private void setOuterId(ShoppingCart shoppingCart, CartItem cartItem) {
+        if (!shoppingCart.getCartItems().isEmpty()) {
+            cartItem.setOuterId(shoppingCart.getCartItems()
+                    .stream()
+                    .map(CartItem::getOuterId).max(Comparator.naturalOrder()).get() + 1);
+        } else {
+            cartItem.setOuterId(1L);
+        }
+    }
+}
