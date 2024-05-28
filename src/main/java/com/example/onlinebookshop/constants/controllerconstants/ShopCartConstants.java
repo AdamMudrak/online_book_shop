5457e349ebd29d85fa8c8735e2b19525cde2b4ef
AdamMudrak <mudrets2000@gmail.com>
2024-05-28 22:18:15 +0300

    Remove unneeded constant

--- src/main/java/com/example/onlinebookshop/constants/controllerconstants/ShopCartConstants.java
+++ src/main/java/com/example/onlinebookshop/constants/controllerconstants/ShopCartConstants.java
@@ -8,13 +8,6 @@ public class ShopCartConstants {
 
     public static final String GET_ALL_SUMMARY =
             "Get all items in the shopping cart optionally with pagination and sorting";
-    public static final String GET_ALL_DESCRIPTION = "Returns all items if not provided with"
-            + " page number(page), page size(size) and sort parameter(sort)";
-
-    public static final String PAGEABLE_EXAMPLE = """
-            {"page": 0,
-            "size": 5,
-             "sort": "bookId,DESC"}""";
 
     public static final String ADD_ITEM_SUMMARY = "Add item to the shopping cart";
     public static final String DELETE_ITEM_SUMMARY = "Delete item from the shopping cart";
