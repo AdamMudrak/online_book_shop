0966647985e1003ee3498d4337828f66ead67328
AdamMudrak <mudrets2000@gmail.com>
2024-05-28 21:57:09 +0300

    Improve Dtos

--- src/main/java/com/example/onlinebookshop/dto/cartitem/request/AddCartItemDto.java
+++ src/main/java/com/example/onlinebookshop/dto/cartitem/request/AddCartItemDto.java
@@ -1,21 +1,28 @@
 package com.example.onlinebookshop.dto.cartitem.request;
 
 import com.example.onlinebookshop.constants.Constants;
-import com.example.onlinebookshop.constants.controllerconstants.ShopCartConstants;
+import com.example.onlinebookshop.constants.dtoconstants.CartItemDtoConstants;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record AddCartItemDto(
-        @Schema(name = ShopCartConstants.BOOK_ID, example = Constants.ID_EXAMPLE)
+        @Schema(name = CartItemDtoConstants.BOOK_ID, example = Constants.ID_EXAMPLE)
         @NotNull
         @Positive
-        //todo make sure they are numbers
+        @Digits(integer = 19, fraction = 0)
+        @Max(9223372036854775807L)
         Long bookId,
-        @Schema(name = ShopCartConstants.QUANTITY, example = ShopCartConstants.QUANTITY_EXAMPLE)
+        @Schema(name = CartItemDtoConstants.QUANTITY,
+                example = CartItemDtoConstants.QUANTITY_EXAMPLE,
+                description = CartItemDtoConstants.QUANTITY_DESCRIPTION)
         @NotNull
         @Positive
+        @Digits(integer = 3, fraction = 0)
+        @Max(100)
         int quantity) {
 }
--- src/main/java/com/example/onlinebookshop/dto/cartitem/request/UpdateItemQuantityDto.java
+++ src/main/java/com/example/onlinebookshop/dto/cartitem/request/UpdateItemQuantityDto.java
@@ -1,15 +1,21 @@
 package com.example.onlinebookshop.dto.cartitem.request;
 
-import com.example.onlinebookshop.constants.controllerconstants.ShopCartConstants;
+import com.example.onlinebookshop.constants.dtoconstants.CartItemDtoConstants;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record UpdateItemQuantityDto(
-        @Schema(name = ShopCartConstants.QUANTITY, example = ShopCartConstants.QUANTITY_EXAMPLE)
+        @Schema(name = CartItemDtoConstants.QUANTITY,
+                example = CartItemDtoConstants.QUANTITY_EXAMPLE,
+                description = CartItemDtoConstants.QUANTITY_DESCRIPTION)
         @NotNull
         @Positive
+        @Digits(integer = 3, fraction = 0)
+        @Max(100)
         int quantity) {
 }
