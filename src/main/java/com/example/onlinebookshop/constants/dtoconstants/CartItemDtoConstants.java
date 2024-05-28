7e64a37bbff0f2809a98bf4397e1c56ca69a08c3
AdamMudrak <mudrets2000@gmail.com>
2024-05-28 21:55:32 +0300

    Create DTO specific constants class

---
+++ src/main/java/com/example/onlinebookshop/constants/dtoconstants/CartItemDtoConstants.java
@@ -0,0 +1,10 @@
+package com.example.onlinebookshop.constants.dtoconstants;
+
+public class CartItemDtoConstants {
+    public static final String BOOK_ID = "Book id";
+    public static final String QUANTITY = "Quantity";
+    public static final String QUANTITY_EXAMPLE = "25";
+    public static final String QUANTITY_DESCRIPTION =
+            "Quantity is limited to a 100 items per client."
+                    + " For more items, please contact us by phone or email.";
+}
