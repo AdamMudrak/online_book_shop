217f9447c30b949a5f612812ec57affb99198dc2
AdamMudrak <mudrets2000@gmail.com>
2024-05-29 13:16:03 +0300

    Import cascade type

--- src/main/java/com/example/onlinebookshop/entities/ShoppingCart.java
+++ src/main/java/com/example/onlinebookshop/entities/ShoppingCart.java
@@ -1,5 +1,6 @@
 package com.example.onlinebookshop.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
