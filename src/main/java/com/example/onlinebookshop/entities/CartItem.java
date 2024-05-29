b65109fd5cfd65581043edb4d8ce5f73ed22f00b
AdamMudrak <mudrets2000@gmail.com>
2024-05-29 16:13:19 +0300

    Remove outer_id

--- src/main/java/com/example/onlinebookshop/entities/CartItem.java
+++ src/main/java/com/example/onlinebookshop/entities/CartItem.java
@@ -20,8 +20,6 @@ public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "outer_id")
-    private Long outerId;
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "shopping_cart_id", nullable = false)
     private ShoppingCart shoppingCart;
