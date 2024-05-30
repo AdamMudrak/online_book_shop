adcf311f35b08985d08cfca642c52e3341b56243
AdamMudrak <mudrets2000@gmail.com>
2024-05-30 22:52:26 +0300

    Use @EntityGraph instead of @Query

--- src/main/java/com/example/onlinebookshop/repositories/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/com/example/onlinebookshop/repositories/shoppingcart/ShoppingCartRepository.java
@@ -1,12 +1,10 @@
 package com.example.onlinebookshop.repositories.shoppingcart;
 
 import com.example.onlinebookshop.entities.ShoppingCart;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    @Query(value = "SELECT cart FROM ShoppingCart cart "
-            + "INNER JOIN cart.user user "
-            + "WHERE user.id = :userId")
+    @EntityGraph(attributePaths = {"cartItems", "cartItems.book"})
     ShoppingCart findByUserId(Long userId);
 }
