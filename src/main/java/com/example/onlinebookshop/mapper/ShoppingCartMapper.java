b4bca7a6d49d7fdad6cd51a4175916913d72a777
AdamMudrak <mudrets2000@gmail.com>
2024-05-30 22:49:14 +0300

    Improve mappers

--- src/main/java/com/example/onlinebookshop/mapper/ShoppingCartMapper.java
+++ src/main/java/com/example/onlinebookshop/mapper/ShoppingCartMapper.java
@@ -1,36 +1,13 @@
 package com.example.onlinebookshop.mapper;
 
 import com.example.onlinebookshop.config.MapperConfig;
-import com.example.onlinebookshop.dto.cartitem.response.CartItemDto;
 import com.example.onlinebookshop.dto.shoppingcart.response.ShoppingCartDto;
 import com.example.onlinebookshop.entities.ShoppingCart;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, uses = CartItemMapper.class)
 public interface ShoppingCartMapper {
-    @Mapping(target = "userId", ignore = true)
-    @Mapping(target = "cartItems", ignore = true)
+    @Mapping(source = "user.id", target = "userId")
     ShoppingCartDto toShoppingCartDto(ShoppingCart shoppingCart);
-
-    @AfterMapping
-    default void setUserId(@MappingTarget ShoppingCartDto shoppingCartDto,
-                           ShoppingCart shoppingCart) {
-        shoppingCartDto.setUserId(shoppingCart.getUser().getId());
-    }
-
-    @AfterMapping
-    default void setItems(@MappingTarget ShoppingCartDto shoppingCartDto,
-                          ShoppingCart shoppingCart) {
-        shoppingCart.getCartItems().forEach(cartItem -> {
-            CartItemDto cartItemDto = new CartItemDto();
-            cartItemDto.setId(cartItem.getId());
-            cartItemDto.setBookId(cartItem.getBook().getId());
-            cartItemDto.setBookTitle(cartItem.getBook().getTitle());
-            cartItemDto.setQuantity(cartItem.getQuantity());
-            shoppingCartDto.getCartItems().add(cartItemDto);
-        });
-    }
 }
