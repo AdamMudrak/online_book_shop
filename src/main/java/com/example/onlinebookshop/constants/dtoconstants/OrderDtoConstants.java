236011876ffdc49e875c09d4854724497ec3dff1
AdamMudrak <mudrets2000@gmail.com>
2024-06-24 00:31:16 +0300

    Add constraints and documentation

---
+++ src/main/java/com/example/onlinebookshop/constants/dtoconstants/OrderDtoConstants.java
@@ -0,0 +1,26 @@
+package com.example.onlinebookshop.constants.dtoconstants;
+
+public class OrderDtoConstants {
+    public static final String STATUS_DTO = "status";
+    public static final String STATUS_DTO_EXAMPLE = "1";
+    public static final String STATUS_DTO_RULES = """
+            1 -> mark order as CREATED;
+            2 -> mark order as PENDING_PAYMENT;
+            3 -> mark order as IN_PROGRESS;
+            4 -> mark order as SHIPPED;
+            5 -> mark order as COMPLETED;
+            6 -> mark order as CANCELLED;
+            
+            Status code are from 1 to 6 and should be so,
+            otherwise an error occurs, but you are able
+            to try again.
+            """;
+
+    public static final String SHIPPING_ADDRESS_DESCRIPTION = """
+            If not provided, the address from
+            the user's account will be used.
+            If there is there is nothing
+            in the user's account as well,
+            an error occurs, but you are able
+            to try again.""";
+}
--- src/main/java/com/example/onlinebookshop/dto/order/request/AddressDto.java
+++ src/main/java/com/example/onlinebookshop/dto/order/request/AddressDto.java
@@ -1,4 +1,14 @@
 package com.example.onlinebookshop.dto.order.request;
 
-public record AddressDto(String shippingAddress) {
+import com.example.onlinebookshop.constants.dtoconstants.OrderDtoConstants;
+import com.example.onlinebookshop.constants.dtoconstants.UserDtoConstants;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+public record AddressDto(
+        @Schema(name = UserDtoConstants.SHIPPING_ADDRESS,
+                example = UserDtoConstants.SHIPPING_ADDRESS_EXAMPLE,
+                description = OrderDtoConstants.SHIPPING_ADDRESS_DESCRIPTION)
+        @NotNull
+        String shippingAddress) {
 }
