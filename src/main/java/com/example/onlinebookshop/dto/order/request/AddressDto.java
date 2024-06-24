58b8f8278ffdeb3abdf075b8b3d67dcfa18c2c47
AdamMudrak <mudrets2000@gmail.com>
2024-06-24 17:39:16 +0300

    Move one constants

--- src/main/java/com/example/onlinebookshop/dto/order/request/AddressDto.java
+++ src/main/java/com/example/onlinebookshop/dto/order/request/AddressDto.java
@@ -1,6 +1,6 @@
 package com.example.onlinebookshop.dto.order.request;
 
-import com.example.onlinebookshop.constants.dtoconstants.OrderDtoConstants;
+import com.example.onlinebookshop.constants.controllerconstants.OrderConstants;
 import com.example.onlinebookshop.constants.dtoconstants.UserDtoConstants;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotNull;
 public record AddressDto(
         @Schema(name = UserDtoConstants.SHIPPING_ADDRESS,
                 example = UserDtoConstants.SHIPPING_ADDRESS_EXAMPLE,
-                description = OrderDtoConstants.SHIPPING_ADDRESS_DESCRIPTION)
+                description = OrderConstants.SHIPPING_ADDRESS_DESCRIPTION)
         @NotNull
         String shippingAddress) {
 }
