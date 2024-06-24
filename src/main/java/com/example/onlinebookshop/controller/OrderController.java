910a6d44b2ef0719cd4849aaa982a5e271708f8c
AdamMudrak <mudrets2000@gmail.com>
2024-06-24 21:13:09 +0300

    Add @Validated to validate Ids

--- src/main/java/com/example/onlinebookshop/controller/OrderController.java
+++ src/main/java/com/example/onlinebookshop/controller/OrderController.java
@@ -22,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RequiredArgsConstructor
 @RestController
 @Tag(name = OrderConstants.ORDER_API_NAME,
