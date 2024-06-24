4e2b2dcd5ffca1ed99c2a656932b7f9cc53d6c0a
AdamMudrak <mudrets2000@gmail.com>
2024-06-24 19:33:19 +0300

    Make repo return many results

--- src/main/java/com/example/onlinebookshop/repositories/order/OrderRepository.java
+++ src/main/java/com/example/onlinebookshop/repositories/order/OrderRepository.java
@@ -1,11 +1,11 @@
 package com.example.onlinebookshop.repositories.order;
 
 import com.example.onlinebookshop.entities.Order;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @EntityGraph(attributePaths = {"orderItems", "orderItems.book"})
-    Optional<Order> findByUserId(Long userId);
+    List<Order> findAllByUserId(Long userId);
 }
