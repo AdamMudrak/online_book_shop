fdec6664d83fb47bd4fd0a2bd7e6e9ae44d8459e
AdamMudrak <mudrets2000@gmail.com>
2024-06-26 19:19:28 +0300

    Modify DTO

--- src/main/java/com/example/onlinebookshop/dto/order/request/UpdateOrderDto.java
+++ src/main/java/com/example/onlinebookshop/dto/order/request/UpdateOrderDto.java
@@ -2,21 +2,13 @@ package com.example.onlinebookshop.dto.order.request;
 
 import com.example.onlinebookshop.constants.dtoconstants.OrderDtoConstants;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.NotBlank;
 
 public record UpdateOrderDto(
         @Schema(name = OrderDtoConstants.STATUS_DTO,
                 example = OrderDtoConstants.STATUS_DTO_EXAMPLE,
                 description = OrderDtoConstants.STATUS_DTO_RULES,
                 requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotNull
-        @Positive
-        @Digits(integer = 1, fraction = 0)
-        @Min(1)
-        @Max(6)
-        Integer status) {
+        @NotBlank
+        String status) {
 }
