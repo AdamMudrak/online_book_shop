0796b8a3db1a08a50af6e778ee873337dcd706dc
AdamMudrak <mudrets2000@gmail.com>
2024-07-02 14:11:49 +0300

    Modify liquibase.properties and delete @Test

--- src/test/java/com/example/onlinebookshop/OnlineBookShopApplicationTests.java
+++ src/test/java/com/example/onlinebookshop/OnlineBookShopApplicationTests.java
@@ -1,11 +1,7 @@
 package com.example.onlinebookshop;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OnlineBookShopApplicationTests {
-    @Test
-    void contextLoads() {
-    }
 }
