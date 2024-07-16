1dd36f9153f3163242e25ca1a7c57e04d23eb6c1
AdamMudrak <mudrets2000@gmail.com>
2024-07-16 12:50:13 +0300

    Use better names and structure for sql scripts

--- src/test/resources/testdb/changelog/test-temporary-changes/book/repo/delete-categories-and-books-categories.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM books_categories;
-ALTER TABLE books_categories AUTO_INCREMENT = 1;
-DELETE FROM categories;
-ALTER TABLE categories AUTO_INCREMENT = 1;
-DELETE FROM books;
-ALTER TABLE books AUTO_INCREMENT = 1;
-
