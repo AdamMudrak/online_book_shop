f95f7c1d6b04a5255d31c9ba5405b37094ed3512
AdamMudrak <mudrets2000@gmail.com>
2024-08-06 00:51:26 +0300

    Add getAll test, use @Sql instead of @BeforeAll, @AfterAll - because otherwise tests cannot pass repeatedly

--- src/test/java/com/example/onlinebookshop/controllers/BookControllerTest.java
+++ src/test/java/com/example/onlinebookshop/controllers/BookControllerTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,22 +21,18 @@ import com.example.onlinebookshop.dtos.book.request.UpdateBookDto;
 import com.example.onlinebookshop.dtos.book.response.BookDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
-import javax.sql.DataSource;
-import lombok.SneakyThrows;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -49,8 +46,11 @@ public class BookControllerTest {
     private static final CreateBookDto CREATE_INVALID_BOOK_DTO = new CreateBookDto();
     private static final UpdateBookDto UPDATE_VALID_BOOK_DTO = new UpdateBookDto();
     private static final UpdateBookDto UPDATE_INVALID_BOOK_DTO = new UpdateBookDto();
-    private static final BookDto EXPECTED_BOOK_DTO = new BookDto();
+    private static final BookDto EXPECTED_NEW_BOOK_DTO = new BookDto();
+    private static final BookDto EXPECTED_GATSBY_BOOK_DTO = new BookDto();
     private static final BookDto EXPECTED_BOOK_DTO_AFTER_UPDATE = new BookDto();
+    private static final BookDto EXPECTED_TKAM_BOOK_DTO = new BookDto();
+    private static final BookDto EXPECTED_1984_BOOK_DTO = new BookDto();
     private static final long EXPECTED_BOOK_DTO_ID = 4L;
     private static final long CATEGORY_ID = 1L;
     private static final String SOME_TITLE = "Some book";
@@ -61,7 +61,6 @@ public class BookControllerTest {
     private static final String SOME_COVER_IMAGE = "https://example.com/updated-cover-image.jpg";
 
     private static final long RANDOM_ID = 1000L;
-    private static final long ID_1984 = 3L;
 
     private static final long GATSBY_ID = 1L;
     private static final String GATSBY_TITLE = "The Great Gatsby";
@@ -71,6 +70,25 @@ public class BookControllerTest {
     private static final String GATSBY_DESCRIPTION = "A story of the fabulously wealthy Jay Gatsby "
             + "and his love for the beautiful Daisy Buchanan.";
     private static final String GATSBY_COVER_IMAGE = "https://example.com/gatsby.jpg";
+
+    private static final long TKAM_ID = 2L;
+    private static final String TKAM_TITLE = "To Kill a Mockingbird";
+    private static final String TKAM_AUTHOR = "Harper Lee";
+    private static final String TKAM_ISBN = "9780061120084";
+    private static final BigDecimal TKAM_PRICE = BigDecimal.valueOf(10.49);
+    private static final String TKAM_DESCRIPTION = "A novel that explores the irrationality of "
+            + "adult attitudes towards race and class in the Deep South of the 1930s.";
+    private static final String TKAM_COVER_IMAGE = "https://example.com/mockingbird.jpg";
+
+    private static final long ID_1984 = 3L;
+    private static final String TITLE_1984 = "1984";
+    private static final String AUTHOR_1984 = "George Orwell";
+    private static final String ISBN_1984 = "9780451524935";
+    private static final BigDecimal PRICE_1984 = BigDecimal.valueOf(9.99);
+    private static final String DESCRIPTION_1984 = "A dystopian novel set in Airstrip One, "
+            + "a province of the superstate Oceania, whose residents are victims of perpetual war, "
+            + "omnipresent government surveillance, and public manipulation.";
+    private static final String COVER_IMAGE_1984 = "https://example.com/1984.jpg";
 
     private static final String INVALID_FOR_NOT_BLANK = "";
     private static final String INVALID_ISBN_TOO_SHORT = "123456789";
@@ -99,33 +117,17 @@ public class BookControllerTest {
             "description size must be between 0 and 3000",
             "coverImage Invalid format filepath"
     );
-    private static final int SUBSTRING_START = 10;
+    private static final int PAGE_NUMBER = 0;
+    private static final int PAGE_SIZE = 3;
     @Autowired
     private ObjectMapper objectMapper;
 
     @BeforeAll
-    static void initVars(
-            @Autowired DataSource dataSource,
-            @Autowired WebApplicationContext webApplicationContext) {
+    static void initVars(@Autowired WebApplicationContext webApplicationContext) {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(webApplicationContext)
                 .apply(springSecurity())
                 .build();
-        try (Connection connection = dataSource.getConnection()) {
-            connection.setAutoCommit(true);
-            ScriptUtils.executeSqlScript(
-                    connection, new ClassPathResource(PATH_TO_SQL_SCRIPTS
-                            .substring(SUBSTRING_START) + ADD_BOOKS_SQL));
-            ScriptUtils.executeSqlScript(
-                    connection, new ClassPathResource(PATH_TO_SQL_SCRIPTS
-                            .substring(SUBSTRING_START) + ADD_CATEGORIES_SQL));
-            ScriptUtils.executeSqlScript(
-                    connection, new ClassPathResource(
-                            PATH_TO_SQL_SCRIPTS.substring(SUBSTRING_START)
-                                    + ADD_BOOKS_CATEGORIES_SQL));
-        } catch (SQLException e) {
-            throw new RuntimeException("Can't process SQL scripts", e);
-        }
         CREATE_NEW_BOOK_DTO.setTitle(SOME_TITLE);
         CREATE_NEW_BOOK_DTO.setAuthor(SOME_AUTHOR);
         CREATE_NEW_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
@@ -133,6 +135,15 @@ public class BookControllerTest {
         CREATE_NEW_BOOK_DTO.setPrice(SOME_PRICE);
         CREATE_NEW_BOOK_DTO.setDescription(SOME_DESCRIPTION);
         CREATE_NEW_BOOK_DTO.setCoverImage(SOME_COVER_IMAGE);
+
+        EXPECTED_NEW_BOOK_DTO.setId(EXPECTED_BOOK_DTO_ID);
+        EXPECTED_NEW_BOOK_DTO.setTitle(SOME_TITLE);
+        EXPECTED_NEW_BOOK_DTO.setAuthor(SOME_AUTHOR);
+        EXPECTED_NEW_BOOK_DTO.setIsbn(SOME_ISBN);
+        EXPECTED_NEW_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
+        EXPECTED_NEW_BOOK_DTO.setPrice(SOME_PRICE);
+        EXPECTED_NEW_BOOK_DTO.setDescription(SOME_DESCRIPTION);
+        EXPECTED_NEW_BOOK_DTO.setCoverImage(SOME_COVER_IMAGE);
 
         CREATE_EXISTING_BOOK_DTO.setTitle(GATSBY_TITLE);
         CREATE_EXISTING_BOOK_DTO.setAuthor(GATSBY_AUTHOR);
@@ -170,39 +181,44 @@ public class BookControllerTest {
         UPDATE_INVALID_BOOK_DTO.setDescription(INVALID_DESCRIPTION);
         UPDATE_INVALID_BOOK_DTO.setCoverImage(INVALID_COVER_IMAGE_WITHOUT_HTTPS);
 
-        EXPECTED_BOOK_DTO.setId(EXPECTED_BOOK_DTO_ID);
-        EXPECTED_BOOK_DTO.setTitle(SOME_TITLE);
-        EXPECTED_BOOK_DTO.setAuthor(SOME_AUTHOR);
-        EXPECTED_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
-        EXPECTED_BOOK_DTO.setIsbn(SOME_ISBN);
-        EXPECTED_BOOK_DTO.setPrice(SOME_PRICE);
-        EXPECTED_BOOK_DTO.setDescription(SOME_DESCRIPTION);
-        EXPECTED_BOOK_DTO.setCoverImage(SOME_COVER_IMAGE);
-    }
+        EXPECTED_GATSBY_BOOK_DTO.setId(GATSBY_ID);
+        EXPECTED_GATSBY_BOOK_DTO.setTitle(GATSBY_TITLE);
+        EXPECTED_GATSBY_BOOK_DTO.setAuthor(GATSBY_AUTHOR);
+        EXPECTED_GATSBY_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
+        EXPECTED_GATSBY_BOOK_DTO.setIsbn(GATSBY_ISBN);
+        EXPECTED_GATSBY_BOOK_DTO.setPrice(GATSBY_PRICE);
+        EXPECTED_GATSBY_BOOK_DTO.setDescription(GATSBY_DESCRIPTION);
+        EXPECTED_GATSBY_BOOK_DTO.setCoverImage(GATSBY_COVER_IMAGE);
 
-    @AfterAll
-    static void afterAll(@Autowired DataSource dataSource) {
-        tearDown(dataSource);
-    }
+        EXPECTED_TKAM_BOOK_DTO.setId(TKAM_ID);
+        EXPECTED_TKAM_BOOK_DTO.setTitle(TKAM_TITLE);
+        EXPECTED_TKAM_BOOK_DTO.setAuthor(TKAM_AUTHOR);
+        EXPECTED_TKAM_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
+        EXPECTED_TKAM_BOOK_DTO.setIsbn(TKAM_ISBN);
+        EXPECTED_TKAM_BOOK_DTO.setPrice(TKAM_PRICE);
+        EXPECTED_TKAM_BOOK_DTO.setDescription(TKAM_DESCRIPTION);
+        EXPECTED_TKAM_BOOK_DTO.setCoverImage(TKAM_COVER_IMAGE);
 
-    @SneakyThrows
-    static void tearDown(DataSource dataSource) {
-        try (Connection connection = dataSource.getConnection()) {
-            connection.setAutoCommit(true);
-            ScriptUtils.executeSqlScript(connection,
-                    new ClassPathResource(PATH_TO_SQL_SCRIPTS.substring(SUBSTRING_START)
-                            + DELETE_BOOKS_CATEGORIES_SQL));
-            ScriptUtils.executeSqlScript(connection,
-                    new ClassPathResource(PATH_TO_SQL_SCRIPTS.substring(SUBSTRING_START)
-                            + DELETE_CATEGORIES_SQL));
-            ScriptUtils.executeSqlScript(connection,
-                    new ClassPathResource(PATH_TO_SQL_SCRIPTS.substring(SUBSTRING_START)
-                            + DELETE_BOOKS_SQL));
-        }
+        EXPECTED_1984_BOOK_DTO.setId(ID_1984);
+        EXPECTED_1984_BOOK_DTO.setTitle(TITLE_1984);
+        EXPECTED_1984_BOOK_DTO.setAuthor(AUTHOR_1984);
+        EXPECTED_1984_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
+        EXPECTED_1984_BOOK_DTO.setIsbn(ISBN_1984);
+        EXPECTED_1984_BOOK_DTO.setPrice(PRICE_1984);
+        EXPECTED_1984_BOOK_DTO.setDescription(DESCRIPTION_1984);
+        EXPECTED_1984_BOOK_DTO.setCoverImage(COVER_IMAGE_1984);
     }
 
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     @Test
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + ADD_BOOKS_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_BOOKS_CATEGORIES_SQL},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_SQL},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("Given a CreateDto of a book which is not in DB, successfully saves it")
     public void createBook_IsAbleToSaveBookWhichIsNotInDB_Success() throws Exception {
         String jsonRequest = objectMapper.writeValueAsString(CREATE_NEW_BOOK_DTO);
@@ -214,11 +230,19 @@ public class BookControllerTest {
                 .andReturn();
         BookDto actual = objectMapper.readValue(
                 result.getResponse().getContentAsString(), BookDto.class);
-        assertEquals(EXPECTED_BOOK_DTO, actual);
+        assertEquals(EXPECTED_NEW_BOOK_DTO, actual);
     }
 
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     @Test
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + ADD_BOOKS_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_BOOKS_CATEGORIES_SQL},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_SQL},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("Given an invalid CreateDto with a mistake for each field, fails to save it")
     public void createBook_IsNotAbleToSaveBookWhenCreateDtoIsInvalid_Fail() throws Exception {
         String jsonRequest = objectMapper.writeValueAsString(CREATE_INVALID_BOOK_DTO);
@@ -236,6 +260,14 @@ public class BookControllerTest {
 
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     @Test
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + ADD_BOOKS_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_BOOKS_CATEGORIES_SQL},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_SQL},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("Given an UpdateDto of a book which is in DB, successfully updates it")
     public void update_IsAbleToUpdateBookWhenUpdateDtoIsValid_Success() throws Exception {
         String jsonRequest = objectMapper.writeValueAsString(UPDATE_VALID_BOOK_DTO);
@@ -252,6 +284,14 @@ public class BookControllerTest {
 
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     @Test
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + ADD_BOOKS_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_BOOKS_CATEGORIES_SQL},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_SQL},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("Given an invalid UpdateDto with a mistake for each field, fails to update it")
     public void update_IsNotAbleToUpdateBookWhenUpdateDtoIsInvalid_Fail() throws Exception {
         String jsonRequest = objectMapper.writeValueAsString(UPDATE_INVALID_BOOK_DTO);
@@ -269,6 +309,14 @@ public class BookControllerTest {
 
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     @Test
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + ADD_BOOKS_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_BOOKS_CATEGORIES_SQL},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_SQL},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("Given a valid book ID, successfully deletes book by ID,"
             + " then make sure that this book doesn't exist no more")
     public void delete_IsAbleToDeleteBookWhenBookExistsById_Success() throws Exception {
@@ -289,6 +337,14 @@ public class BookControllerTest {
 
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     @Test
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + ADD_BOOKS_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_BOOKS_CATEGORIES_SQL},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_SQL},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @DisplayName("Given an invalid book ID, fails to delete book by ID")
     public void delete_IsNotAbleToDeleteBookWhenBookDoesNotExistById_Fail() throws Exception {
         String expectedExceptionMessage = "Can't find and delete book by id " + RANDOM_ID;
@@ -299,5 +355,35 @@ public class BookControllerTest {
                 .andReturn();
         String actualExceptionMessage = result.getResponse().getContentAsString();
         assertEquals(expectedExceptionMessage, actualExceptionMessage);
+    }
+
+    @WithMockUser(username = "user")
+    @Test
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + ADD_BOOKS_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + ADD_BOOKS_CATEGORIES_SQL},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_CATEGORIES_SQL,
+            PATH_TO_SQL_SCRIPTS + DELETE_BOOKS_SQL},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("Get all books from DB")
+    public void getAll_IsAbleToGetThreeBooksFromDB_Success() throws Exception {
+        //TODO apply this pageable somehow
+        Pageable pageable = PageRequest.of(PAGE_NUMBER, PAGE_SIZE);
+        List<BookDto> expectedBookDtos = List.of(
+                EXPECTED_GATSBY_BOOK_DTO, EXPECTED_TKAM_BOOK_DTO, EXPECTED_1984_BOOK_DTO);
+        MvcResult result = mockMvc.perform(
+                        get("/books")
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        List<BookDto> actualBookDtos = objectMapper.readValue(result
+                        .getResponse()
+                        .getContentAsString(),
+                objectMapper
+                        .getTypeFactory()
+                        .constructCollectionType(List.class, BookDto.class));
+        assertEquals(expectedBookDtos, actualBookDtos);
     }
 }
