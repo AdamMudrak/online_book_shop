3fa85e42f1bf9741fb0cf334d52827f3932c79ab
AdamMudrak <mudrets2000@gmail.com>
2024-08-05 21:41:42 +0300

    Add tests for create-update-delete

---
+++ src/test/java/com/example/onlinebookshop/controllers/BookControllerTest.java
@@ -0,0 +1,303 @@
+package com.example.onlinebookshop.controllers;
+
+import static com.example.onlinebookshop.TestConstants.ADD_BOOKS_CATEGORIES_SQL;
+import static com.example.onlinebookshop.TestConstants.ADD_BOOKS_SQL;
+import static com.example.onlinebookshop.TestConstants.ADD_CATEGORIES_SQL;
+import static com.example.onlinebookshop.TestConstants.DELETE_BOOKS_CATEGORIES_SQL;
+import static com.example.onlinebookshop.TestConstants.DELETE_BOOKS_SQL;
+import static com.example.onlinebookshop.TestConstants.DELETE_CATEGORIES_SQL;
+import static com.example.onlinebookshop.TestConstants.PATH_TO_SQL_SCRIPTS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.onlinebookshop.dtos.book.request.CreateBookDto;
+import com.example.onlinebookshop.dtos.book.request.UpdateBookDto;
+import com.example.onlinebookshop.dtos.book.response.BookDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Set;
+import javax.sql.DataSource;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BookControllerTest {
+    protected static MockMvc mockMvc;
+    private static final CreateBookDto CREATE_NEW_BOOK_DTO = new CreateBookDto();
+    private static final CreateBookDto CREATE_EXISTING_BOOK_DTO = new CreateBookDto();
+    private static final CreateBookDto CREATE_INVALID_BOOK_DTO = new CreateBookDto();
+    private static final UpdateBookDto UPDATE_VALID_BOOK_DTO = new UpdateBookDto();
+    private static final UpdateBookDto UPDATE_INVALID_BOOK_DTO = new UpdateBookDto();
+    private static final BookDto EXPECTED_BOOK_DTO = new BookDto();
+    private static final BookDto EXPECTED_BOOK_DTO_AFTER_UPDATE = new BookDto();
+    private static final long EXPECTED_BOOK_DTO_ID = 4L;
+    private static final long CATEGORY_ID = 1L;
+    private static final String SOME_TITLE = "Some book";
+    private static final String SOME_AUTHOR = "Some author";
+    private static final String SOME_ISBN = "0000000000000";
+    private static final BigDecimal SOME_PRICE = BigDecimal.valueOf(9.99);
+    private static final String SOME_DESCRIPTION = "Some description";
+    private static final String SOME_COVER_IMAGE = "https://example.com/updated-cover-image.jpg";
+
+    private static final long RANDOM_ID = 1000L;
+    private static final long ID_1984 = 3L;
+
+    private static final long GATSBY_ID = 1L;
+    private static final String GATSBY_TITLE = "The Great Gatsby";
+    private static final String GATSBY_AUTHOR = "F. Scott Fitzgerald";
+    private static final String GATSBY_ISBN = "9780743273565";
+    private static final BigDecimal GATSBY_PRICE = BigDecimal.valueOf(12.99);
+    private static final String GATSBY_DESCRIPTION = "A story of the fabulously wealthy Jay Gatsby "
+            + "and his love for the beautiful Daisy Buchanan.";
+    private static final String GATSBY_COVER_IMAGE = "https://example.com/gatsby.jpg";
+
+    private static final String INVALID_FOR_NOT_BLANK = "";
+    private static final String INVALID_ISBN_TOO_SHORT = "123456789";
+    private static final String VALID_ISBN = "1234567890";
+    private static final long INVALID_CATEGORY_ID_DOES_NOT_EXIST = 2L;
+    private static final BigDecimal VALID_PRICE = BigDecimal.valueOf(1);
+    private static final BigDecimal INVALID_PRICE_NEGATIVE = BigDecimal.valueOf(-1);
+    private static final int REPEAT_DESCRIPTION_COUNT = 100;
+    private static final String INVALID_DESCRIPTION =
+            "This text contains more than 300 characters.".repeat(REPEAT_DESCRIPTION_COUNT);
+    private static final String VALID_DESCRIPTION =
+            "This text contains more than 0 and less than 300 characters.";
+    private static final String INVALID_COVER_IMAGE_WITHOUT_HTTPS = "example.com/gatsby.jpg";
+    private static final String VALID_COVER_IMAGE = "https://example.com/another_gatsby.jpg";
+    private static final List<String> CREATE_ERRORS_LIST = List.of(
+            "title must not be blank",
+            "author must not be blank",
+            "isbn size must be between 10 and 17",
+            "price must be greater than 0",
+            "description size must be between 0 and 3000",
+            "coverImage Invalid format filepath"
+           );
+    private static final List<String> UPDATE_ERRORS_LIST = List.of(
+            "isbn size must be between 10 and 17",
+            "price must be greater than 0",
+            "description size must be between 0 and 3000",
+            "coverImage Invalid format filepath"
+    );
+    private static final int SUBSTRING_START = 10;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeAll
+    static void initVars(
+            @Autowired DataSource dataSource,
+            @Autowired WebApplicationContext webApplicationContext) {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(
+                    connection, new ClassPathResource(PATH_TO_SQL_SCRIPTS
+                            .substring(SUBSTRING_START) + ADD_BOOKS_SQL));
+            ScriptUtils.executeSqlScript(
+                    connection, new ClassPathResource(PATH_TO_SQL_SCRIPTS
+                            .substring(SUBSTRING_START) + ADD_CATEGORIES_SQL));
+            ScriptUtils.executeSqlScript(
+                    connection, new ClassPathResource(
+                            PATH_TO_SQL_SCRIPTS.substring(SUBSTRING_START)
+                                    + ADD_BOOKS_CATEGORIES_SQL));
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't process SQL scripts", e);
+        }
+        CREATE_NEW_BOOK_DTO.setTitle(SOME_TITLE);
+        CREATE_NEW_BOOK_DTO.setAuthor(SOME_AUTHOR);
+        CREATE_NEW_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
+        CREATE_NEW_BOOK_DTO.setIsbn(SOME_ISBN);
+        CREATE_NEW_BOOK_DTO.setPrice(SOME_PRICE);
+        CREATE_NEW_BOOK_DTO.setDescription(SOME_DESCRIPTION);
+        CREATE_NEW_BOOK_DTO.setCoverImage(SOME_COVER_IMAGE);
+
+        CREATE_EXISTING_BOOK_DTO.setTitle(GATSBY_TITLE);
+        CREATE_EXISTING_BOOK_DTO.setAuthor(GATSBY_AUTHOR);
+        CREATE_EXISTING_BOOK_DTO.setIsbn(GATSBY_ISBN);
+        CREATE_EXISTING_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
+        CREATE_EXISTING_BOOK_DTO.setPrice(GATSBY_PRICE);
+        CREATE_EXISTING_BOOK_DTO.setDescription(GATSBY_DESCRIPTION);
+        CREATE_EXISTING_BOOK_DTO.setCoverImage(GATSBY_COVER_IMAGE);
+
+        EXPECTED_BOOK_DTO_AFTER_UPDATE.setId(GATSBY_ID);
+        EXPECTED_BOOK_DTO_AFTER_UPDATE.setTitle(GATSBY_TITLE);
+        EXPECTED_BOOK_DTO_AFTER_UPDATE.setAuthor(GATSBY_AUTHOR);
+        EXPECTED_BOOK_DTO_AFTER_UPDATE.setCategoryIds(Set.of(CATEGORY_ID));
+        EXPECTED_BOOK_DTO_AFTER_UPDATE.setIsbn(VALID_ISBN);
+        EXPECTED_BOOK_DTO_AFTER_UPDATE.setPrice(VALID_PRICE);
+        EXPECTED_BOOK_DTO_AFTER_UPDATE.setDescription(VALID_DESCRIPTION);
+        EXPECTED_BOOK_DTO_AFTER_UPDATE.setCoverImage(VALID_COVER_IMAGE);
+
+        CREATE_INVALID_BOOK_DTO.setTitle(INVALID_FOR_NOT_BLANK);
+        CREATE_INVALID_BOOK_DTO.setAuthor(INVALID_FOR_NOT_BLANK);
+        CREATE_INVALID_BOOK_DTO.setIsbn(INVALID_ISBN_TOO_SHORT);
+        CREATE_INVALID_BOOK_DTO.setCategoryIds(Set.of(INVALID_CATEGORY_ID_DOES_NOT_EXIST));
+        CREATE_INVALID_BOOK_DTO.setPrice(INVALID_PRICE_NEGATIVE);
+        CREATE_INVALID_BOOK_DTO
+                .setDescription(INVALID_DESCRIPTION);
+        CREATE_INVALID_BOOK_DTO.setCoverImage(INVALID_COVER_IMAGE_WITHOUT_HTTPS);
+
+        UPDATE_VALID_BOOK_DTO.setIsbn(VALID_ISBN);
+        UPDATE_VALID_BOOK_DTO.setPrice(VALID_PRICE);
+        UPDATE_VALID_BOOK_DTO.setDescription(VALID_DESCRIPTION);
+        UPDATE_VALID_BOOK_DTO.setCoverImage(VALID_COVER_IMAGE);
+
+        UPDATE_INVALID_BOOK_DTO.setIsbn(INVALID_ISBN_TOO_SHORT);
+        UPDATE_INVALID_BOOK_DTO.setPrice(INVALID_PRICE_NEGATIVE);
+        UPDATE_INVALID_BOOK_DTO.setDescription(INVALID_DESCRIPTION);
+        UPDATE_INVALID_BOOK_DTO.setCoverImage(INVALID_COVER_IMAGE_WITHOUT_HTTPS);
+
+        EXPECTED_BOOK_DTO.setId(EXPECTED_BOOK_DTO_ID);
+        EXPECTED_BOOK_DTO.setTitle(SOME_TITLE);
+        EXPECTED_BOOK_DTO.setAuthor(SOME_AUTHOR);
+        EXPECTED_BOOK_DTO.setCategoryIds(Set.of(CATEGORY_ID));
+        EXPECTED_BOOK_DTO.setIsbn(SOME_ISBN);
+        EXPECTED_BOOK_DTO.setPrice(SOME_PRICE);
+        EXPECTED_BOOK_DTO.setDescription(SOME_DESCRIPTION);
+        EXPECTED_BOOK_DTO.setCoverImage(SOME_COVER_IMAGE);
+    }
+
+    @AfterAll
+    static void afterAll(@Autowired DataSource dataSource) {
+        tearDown(dataSource);
+    }
+
+    @SneakyThrows
+    static void tearDown(DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(connection,
+                    new ClassPathResource(PATH_TO_SQL_SCRIPTS.substring(SUBSTRING_START)
+                            + DELETE_BOOKS_CATEGORIES_SQL));
+            ScriptUtils.executeSqlScript(connection,
+                    new ClassPathResource(PATH_TO_SQL_SCRIPTS.substring(SUBSTRING_START)
+                            + DELETE_CATEGORIES_SQL));
+            ScriptUtils.executeSqlScript(connection,
+                    new ClassPathResource(PATH_TO_SQL_SCRIPTS.substring(SUBSTRING_START)
+                            + DELETE_BOOKS_SQL));
+        }
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("Given a CreateDto of a book which is not in DB, successfully saves it")
+    public void createBook_IsAbleToSaveBookWhichIsNotInDB_Success() throws Exception {
+        String jsonRequest = objectMapper.writeValueAsString(CREATE_NEW_BOOK_DTO);
+        MvcResult result = mockMvc.perform(
+                            post("/books")
+                                    .content(jsonRequest)
+                                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+        BookDto actual = objectMapper.readValue(
+                result.getResponse().getContentAsString(), BookDto.class);
+        assertEquals(EXPECTED_BOOK_DTO, actual);
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("Given an invalid CreateDto with a mistake for each field, fails to save it")
+    public void createBook_IsNotAbleToSaveBookWhenCreateDtoIsInvalid_Fail() throws Exception {
+        String jsonRequest = objectMapper.writeValueAsString(CREATE_INVALID_BOOK_DTO);
+        MvcResult result = mockMvc.perform(
+                        post("/books")
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        String actualExceptionMessage = result.getResponse().getContentAsString();
+        for (String error : CREATE_ERRORS_LIST) {
+            assertTrue(actualExceptionMessage.contains(error));
+        }
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("Given an UpdateDto of a book which is in DB, successfully updates it")
+    public void update_IsAbleToUpdateBookWhenUpdateDtoIsValid_Success() throws Exception {
+        String jsonRequest = objectMapper.writeValueAsString(UPDATE_VALID_BOOK_DTO);
+        MvcResult result = mockMvc.perform(
+                        put("/books/" + GATSBY_ID)
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        BookDto actual = objectMapper.readValue(
+                result.getResponse().getContentAsString(), BookDto.class);
+        assertEquals(EXPECTED_BOOK_DTO_AFTER_UPDATE, actual);
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("Given an invalid UpdateDto with a mistake for each field, fails to update it")
+    public void update_IsNotAbleToUpdateBookWhenUpdateDtoIsInvalid_Fail() throws Exception {
+        String jsonRequest = objectMapper.writeValueAsString(UPDATE_INVALID_BOOK_DTO);
+        MvcResult result = mockMvc.perform(
+                        put("/books/" + GATSBY_ID)
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        String actualExceptionMessage = result.getResponse().getContentAsString();
+        for (String error : UPDATE_ERRORS_LIST) {
+            assertTrue(actualExceptionMessage.contains(error));
+        }
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("Given a valid book ID, successfully deletes book by ID,"
+            + " then make sure that this book doesn't exist no more")
+    public void delete_IsAbleToDeleteBookWhenBookExistsById_Success() throws Exception {
+        mockMvc.perform(
+                        delete("/books/" + ID_1984)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        String expectedExceptionMessage = "Can't find and delete book by id " + ID_1984;
+        MvcResult result = mockMvc.perform(
+                        delete("/books/" + ID_1984)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        String actualExceptionMessage = result.getResponse().getContentAsString();
+        assertEquals(expectedExceptionMessage, actualExceptionMessage);
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("Given an invalid book ID, fails to delete book by ID")
+    public void delete_IsNotAbleToDeleteBookWhenBookDoesNotExistById_Fail() throws Exception {
+        String expectedExceptionMessage = "Can't find and delete book by id " + RANDOM_ID;
+        MvcResult result = mockMvc.perform(
+                        delete("/books/" + RANDOM_ID)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        String actualExceptionMessage = result.getResponse().getContentAsString();
+        assertEquals(expectedExceptionMessage, actualExceptionMessage);
+    }
+}
