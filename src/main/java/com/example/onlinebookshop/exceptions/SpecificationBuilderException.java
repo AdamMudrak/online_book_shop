0b94e43a0f2a3a2522b118ad67292ccdb94e6f04
AdamMudrak <mudrets2000@gmail.com>
2024-08-06 19:05:08 +0300

    Add an exception to prevent searching with non-existing parameters

---
+++ src/main/java/com/example/onlinebookshop/exceptions/SpecificationBuilderException.java
@@ -0,0 +1,7 @@
+package com.example.onlinebookshop.exceptions;
+
+public class SpecificationBuilderException extends RuntimeException {
+    public SpecificationBuilderException(String message) {
+        super(message);
+    }
+}
