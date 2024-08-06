51e636120af2ca917887d4806755a2e7094dd5ec
AdamMudrak <mudrets2000@gmail.com>
2024-08-06 20:20:37 +0300

    Add more categories to test SQL

--- src/test/resources/testdb/changelog/test-temporary-changes/repositories/add-categories.sql
+++ src/test/resources/testdb/changelog/test-temporary-changes/repositories/add-categories.sql
@@ -1,3 +1,5 @@
 INSERT INTO categories (name, description)
 VALUES
-    ('Fiction', 'Interesting books about imaginary though possible events');
+    ('Fiction', 'Interesting books about imaginary though possible events'),
+    ('Science Fiction', 'Interesting books about imaginary though possible events in the distant future'),
+    ('Non-fiction', 'Books about real events');
